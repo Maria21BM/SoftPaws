834f41ce3146cc8dd84c935368e74beabd989317
Maria Buliga <mariaeliza2191@gmail.com>
2025-10-28 14:01:47 +0200

    Rename PetData to CatData

--- app/src/main/java/com/mariabuliga/softpaws/CatAdapter.kt
+++ app/src/main/java/com/mariabuliga/softpaws/CatAdapter.kt
@@ -5,24 +5,24 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
-import com.mariabuliga.softpaws.databinding.PetItemBinding
-import com.mariabuliga.softpaws.models.PetData
+import com.mariabuliga.softpaws.databinding.CatItemBinding
+import com.mariabuliga.softpaws.models.CatData
 
-class PetAdapter() : RecyclerView.Adapter<PetAdapter.PetViewHolder>() {
+class CatAdapter() : RecyclerView.Adapter<CatAdapter.PetViewHolder>() {
 
-    inner class PetViewHolder(val binding: PetItemBinding) : RecyclerView.ViewHolder(binding.root)
+    inner class PetViewHolder(val binding: CatItemBinding) : RecyclerView.ViewHolder(binding.root)
 
-    private val diffUtil = object : DiffUtil.ItemCallback<PetData>() {
+    private val diffUtil = object : DiffUtil.ItemCallback<CatData>() {
         override fun areItemsTheSame(
-            oldItem: PetData,
-            newItem: PetData
+            oldItem: CatData,
+            newItem: CatData
         ): Boolean {
             return oldItem.id == newItem.id && oldItem.name == newItem.name
         }
 
         override fun areContentsTheSame(
-            oldItem: PetData,
-            newItem: PetData
+            oldItem: CatData,
+            newItem: CatData
         ): Boolean {
             return oldItem == newItem
         }
@@ -30,7 +30,7 @@ class PetAdapter() : RecyclerView.Adapter<PetAdapter.PetViewHolder>() {
 
     private val asyncListDiffer = AsyncListDiffer(this, diffUtil)
 
-    fun saveData(data: List<PetData>){
+    fun saveData(data: List<CatData>){
         asyncListDiffer.submitList(data)
     }
 
@@ -38,7 +38,7 @@ class PetAdapter() : RecyclerView.Adapter<PetAdapter.PetViewHolder>() {
         parent: ViewGroup,
         viewType: Int
     ): PetViewHolder {
-        val binding = PetItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding = CatItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return PetViewHolder(binding)
     }
 
